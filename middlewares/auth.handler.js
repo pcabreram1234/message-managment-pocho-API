04853bd2571b9e4525d33acee1200fdf9e82d0d5
Phillip <pcabreram@edenorte.com.do>
2024-11-24 18:58:38 -0400

    More posible way to receive the token prop from the frontEnd added inth auth.handler midleware

--- middlewares/auth.handler.js
+++ middlewares/auth.handler.js
@@ -3,6 +3,11 @@ const { models } = require("../libs/sequelize");
 
 async function verifyToken(req, res, next) {
   const token = req.cookies?.token;
+  req.headers["authorization"] ||
+    req.headers["token"] ||
+    req.body.token ||
+    req.query.token ||
+    req.headers["x-access-token"];
 
   if (!token) {
     return res.status(403).send("A token is required for authentication");
