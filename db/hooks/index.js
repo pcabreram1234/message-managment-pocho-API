1261e48b55d3c2240a7e735801b5307be018c2aa
Phillip Leonardo Cabrera Medrano <pcabreram@edenorte.com.do>
2025-07-08 21:28:13 -0400

    DB Hook: initBDHOOKS
    - Added but commented the initCampaignRecipientHooks.

--- db/hooks/index.js
+++ db/hooks/index.js
@@ -1,11 +1,13 @@
 const { initContactHooks } = require("./Contact");
 const { initCategoryHooks } = require("./Category");
 const { initMessageConfigHooks } = require("./MessageConfig");
+const { initCampaignRecipientHooks } = require("./CampaignsRecipients");
 
 const initBDHooks = () => {
   initContactHooks();
   initCategoryHooks();
   initMessageConfigHooks();
+  // initCampaignRecipientHooks();
 };
 
 module.exports = { initBDHooks };
