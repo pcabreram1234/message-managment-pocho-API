1fe009bdccb6ab71d41fd8750c1094c740207fc9
Phillip Leonardo Cabrera Medrano <pcabreram@edenorte.com.do>
2025-07-09 09:22:40 -0400

    Modified Model: CampaignRecipient
    - Added the fields createdAt and updatedAt to properly manage the audit of data
    - Added the options active, paused, completed and cancelled in the status field of the camapaign_recipient table

--- db/models/CampaignRecipients.js
+++ db/models/CampaignRecipients.js
@@ -1,17 +1,36 @@
-const { Model, DataTypes } = require("sequelize");
+const { Model, DataTypes, Sequelize } = require("sequelize");
 const CAMPAIGN_RECIPIENTS_TABLE = "campaign_recipients";
 
 const CampaignRecipientModel = {
   campaign_id: DataTypes.INTEGER,
-  status: DataTypes.ENUM("pending", "sending", "sent", "failed"),
+  status: DataTypes.ENUM(
+    "pending",
+    "active",
+    "paused",
+    "completed",
+    "cancelled"
+  ),
   last_attempt_at: DataTypes.DATE,
   error_message: DataTypes.STRING,
+  createdAt: {
+    allowNull: false,
+    type: "timestamp",
+    field: "created_at",
+    defaultValue: Sequelize.fn("CURRENT_TIMESTAMP"),
+  },
+  updatedAt: {
+    allowNull: false,
+    type: "timestamp",
+    field: "updated_at",
+    defaultValue: Sequelize.fn("NOW"),
+    onUpdate: Sequelize.literal("CURRENT_TIMESTAMP"),
+  },
 };
 
 class CampaignRecipient extends Model {
   static associate(models) {
     this.belongsTo(models.Campaign, { foreignKey: "campaign_id" });
-    this.belongsTo(models.Contact)
+    this.belongsTo(models.Contact);
   }
   static config(sequelize) {
     return {
