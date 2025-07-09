3ce6f5bcf1e02110fcc34e7da3a868f49826be47
root <pcabreram@edenorte.com.do>
2025-07-09 09:57:51 -0400

    Modified Model: MessageLog
    - Added fields created in and updated in to properly manage data auditing

--- db/models/MessageLog.js
+++ db/models/MessageLog.js
@@ -1,4 +1,4 @@
-const { Model, DataTypes } = require("sequelize");
+const { Model, DataTypes, Sequelize } = require("sequelize");
 const MESSAGE_LOG_TABLE = "message_logs";
 
 const MessageLogModel = {
@@ -9,6 +9,19 @@ const MessageLogModel = {
   status: DataTypes.ENUM("success", "failed", "retrying"),
   attempts: DataTypes.INTEGER,
   error_message: DataTypes.STRING,
+  createdAt: {
+    allowNull: false,
+    type: "timestamp",
+    field: "created_at",
+    defaultValue: Sequelize.fn("CURRENT_TIMESTAMP"),
+  },
+  updatedAt: {
+    allowNull: false,
+    type: "timestamp",
+    field: "updated_at",
+    defaultValue: Sequelize.fn("NOW"),
+    onUpdate: Sequelize.literal("CURRENT_TIMESTAMP"),
+  },
 };
 
 class MessageLog extends Model {
